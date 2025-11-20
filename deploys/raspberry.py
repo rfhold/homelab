810529b39c8c42116e39c70b74192af3a70858ca
rfhold <rfhold@rholden.dev>
2025-11-20 13:37:56 -0500

    rpi locals

--- deploys/raspberry.py
+++ deploys/raspberry.py
@@ -1,4 +1,4 @@
-from pyinfra.operations import files
+from pyinfra.operations import files, apt
 
 _ = files.replace(
     name="Enable cgroups in boot config",
@@ -7,4 +7,10 @@ _ = files.replace(
     text="$",  # Match end of line
     replace=" cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory",  # Text to append
     backup=True,
+)
+
+apt.packages(
+    name="Install locales-all package",
+    packages=["locales-all"],
+    _sudo=True,
 )
